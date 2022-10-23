b448f1e35bfbc7ee33a089dd49d6213e79f36f32
Patryk Kowalczyk <noxworl2k@gmail.com>
2022-10-23 09:26:23 +0200

    Task done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,5 @@
+
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -5,6 +7,34 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent{
   title = 'psitagram';
+  apiURL = 'https://dog.ceo/api/breeds/list/all';
+  dogsData: any;
+  dogsList: string[] = [];
+  dog: string = '';
+  dogImage: any;
+  dogWikiUrl: string = '';
+
+
+
+  constructor(private http: HttpClient) {}
+  ngOnInit(): void{
+    this.http.get(this.apiURL).subscribe((data) => {
+      this.dogsData = data;
+      this.dogsData = this.dogsData.message;
+      this.dogsList = Object.keys(this.dogsData);
+      console.log(this.dogsData);
+    });
+  }
+  onChange(event: any){
+    this.dog = event.target.value
+     const apiReqUrl = `https://dog.ceo/api/breed/${this.dog}/images`;
+    this.http.get(apiReqUrl).subscribe((data) => {
+      this.dogImage = data;
+      this.dogImage = this.dogImage.message[0];
+      this.dogWikiUrl = `https://pl.wikipedia.org/wiki/${this.dog}`;
+      }
+    );
+  }
 }
